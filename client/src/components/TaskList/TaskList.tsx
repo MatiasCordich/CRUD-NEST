8002eea4398cb85cb84cd14cbe7907062015a261
MatiasCordich <m.scordich@gmail.com>
2023-10-14 22:49:22 -0300

    fixien taskList

--- client/src/components/TaskList/TaskList.tsx
+++ client/src/components/TaskList/TaskList.tsx
@@ -5,35 +5,34 @@ import { NoTasksMessage, TaskListBox } from './TaskListElements'
 const TaskList = (): JSX.Element => {
 
   const { tasks } = useTasks()
+    return (
+      <>
+        {!tasks.length
+          ? <NoTasksMessage
+            initial={{ opacity: 0, y: "-2rem" }}
+            animate={{ opacity: 1, y: "0rem" }}
+            transition={{ duration: 1, delay: 1.1, }}
+          >
+            Aun no hay tareas
+          </NoTasksMessage>
+          : <TaskListBox
+            initial={{ opacity: 0, y: "-2rem" }}
+            animate={{ opacity: 1, y: "0rem" }}
+            transition={{ duration: 1, delay: 2.5 }}
+          >
 
-  if (!tasks.length) {
-    <>
-      <NoTasksMessage
-        initial={{ opacity: 0, y: "-2rem" }}
-        animate={{ opacity: 1, y: "0rem" }}
-        transition={{ duration: 1, delay: 1.1, }}
-      >
-        Aun no hay tareas
-      </NoTasksMessage>;
-    </>
-  }
+            {
+              tasks.map(task => (
+                <TaskItem task={task} key={task._id} />
+              ))
+            }
+          </TaskListBox>}
+      </>
 
-
-
-  return (
-    <TaskListBox
-      initial={{ opacity: 0, y: "-2rem" }}
-      animate={{ opacity: 1, y: "0rem" }}
-      transition={{ duration: 1, delay: 2.5 }}
-    >
-
-      {
-        tasks.map(task => (
-          <TaskItem task={task} key={task._id} />
-        ))
-      }
-    </TaskListBox>
-  )
+    )
+  
 }
+
+
 
 export default TaskList
